582b4f1d10c62704c7fdf03d4581b50b6ee5826a
AurelienNioche <nioche.aurelien@gmail.com>
2022-06-07 14:23:39 +0100

    update

---
+++ agents/assistants.py
@@ -0,0 +1,29 @@
+import numpy as np
+import gym
+from gym import spaces
+
+
+class Assistant(gym.env):
+
+    def __init__():
+        self.n_targets = n_targets
+        self.goal = None  # index of preferred target, assigned during reset()
+
+    def step(self, action):
+        return observation, reward, done, info
+
+    def reset(self):
+        return observation  # reward, done, info can't be included
+
+    def render(self, mode='human'):
+
+    def close(self):
+
+
+def main():
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
