8666e8f335f7ffd39853b6070dfa789dc6200ff9
AurelienNioche <nioche.aurelien@gmail.com>
2022-07-23 21:14:13 +0100

    update

--- src/scenario2_alt/main.py
+++ src/scenario2_alt/main.py
@@ -18,21 +18,49 @@ except ModuleNotFoundError:
 
 def main():
 
+    # decision_rule_kwargs = dict(
+    #     decision_rule='softmax',
+    #     decision_rule_parameters=dict(
+    #         temperature=100.0)
+    # )
+
+    n_targets = 10
+
+    decision_rule_kwargs = dict(
+        decision_rule='active_inference',
+        decision_rule_parameters=dict(
+            decay_factor=0.9,
+            n_rollout=None,
+            n_step_per_rollout=1)
+    )
+
+    # decision_rule_kwargs = dict(
+    #     decision_rule='epsilon_rule',
+    #     decision_rule_parameters=dict(
+    #         epsilon=0.2)
+    # )
+
+    # decision_rule_kwargs = dict(
+    #     decision_rule="random"
+    # )
+
+    run_name = f"{decision_rule_kwargs['decision_rule']}_{n_targets}targets"
+
     trace = run(
         user_model=User,
         assistant_model=AiAssistant,
         seed=1,
         user_parameters=(8.0, 0.5),
-        n_step=2000,
-        n_targets=10,
+        n_step=1000,
+        n_targets=n_targets,
+        inference_max_epochs=500,
+        inference_learning_rate=0.1,
         target_closer_step_size=0.01,
         target_further_step_size=None,
-        decision_rule='active_inference',
-        decision_rule_parameters=dict(
-            decay_factor=0.9,
-            n_rollout=5,
-            n_step_per_rollout=2))
-    plot_traces(trace, show=True)
+        **decision_rule_kwargs)
+
+    plot_traces(trace, show=True,
+                run_name=run_name)
 
 
 if __name__ == '__main__':
