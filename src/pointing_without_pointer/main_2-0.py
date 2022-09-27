89c3a066d03e448a2ddbfa03445419dca83532d8
AurelienNioche <nioche.aurelien@gmail.com>
2022-09-27 17:10:16 +0100

    works

--- src/pointing_without_pointer/main_2-0.py
+++ src/pointing_without_pointer/main_2-0.py
@@ -93,7 +93,7 @@ class Model:
         self.old_mouse_pos = np.zeros(2)
         self.old_noise = np.zeros((self.n_target, 2))
 
-        self.selected = False
+        self.selected = np.zeros(self.n_target, dtype=bool)
         self.n_frame_since_selected = 0
         self.disturbance_phase = 0  # Will be incremented
         self.control = np.zeros(2)
@@ -104,6 +104,7 @@ class Model:
         self.mouse_pos_prev_frame = np.zeros(2)
 
         self.initialize()
+        self.reset()
         while True:
             self.loop()
 
@@ -114,22 +115,28 @@ class Model:
             self.window.clear()
             self.window.update()
 
+        self.pos[:] = 0.5
+
+    def reset(self):
+
+        self.var_ratio[:] = 0
+        self.selected[:] = 0
+
         self.window.move_back_cursor_to_the_middle()
 
-        # self.old_mouse_pos[:] = self.window.mouse_position - 0.5
-        # self.old_noise[:] = 0
-
         self.color[:] = self.color_still
-        self.radius[:] = self.base_radius
 
         self.control[:] = 0
+        self.hist_control[:] = 0
         self.mouse_pos_prev_frame[:] = self.window.mouse_position
 
         for i in range(self.n_target):
-            self.pos[i] = 0.5, 0.5
             for coord in range(2):
                 self.hist_pos[i, coord, :] = self.pos[i, coord]
                 self.hist_model[i, coord, :] = self.pos[i, coord]
+
+        self.n_frame_since_selected = 0
+        self.disturbance_phase = 0  # Will be incremented
 
     def loop(self):
 
@@ -147,23 +154,25 @@ class Model:
         self.hist_control = np.roll(self.hist_control, -1)
         self.hist_control[:, -1] = self.control
 
-        self.disturbance_phase += self.phase_inc
-
         self.update_wave()
-        self.update_deriv()
 
-        self.normalize()
+        if self.selected.any():
+            self.n_frame_since_selected += 1
+            if self.n_frame_since_selected == self.n_frame_selected:
+                self.reset()
+        else:
+            self.update_deriv()
 
         self.mouse_pos_prev_frame = mouse_pos
 
     def draw_trans_obj(self):
 
         p_val = np.zeros(self.n_target)
-        selected = np.zeros(self.n_target, dtype=bool)
 
         for i in range(self.n_target):
-            p_val[i] = self.var_ratio[i] / 100000.0
-            selected[i] = p_val[i] >= self.selection_threshold
+            p_val[i] = self.var_ratio[i] / 1e5
+
+        self.selected[:] = p_val >= self.selection_threshold
 
         visual_pos = np.zeros_like(self.pos)
         for coord in range(2):
@@ -173,9 +182,12 @@ class Model:
         visual_pos[:] = np.clip(visual_pos, -100, 100)
         visual_pos[:] = 0.5 + visual_pos / (2*100)
 
+        for coord in range(2):
+            visual_pos[:, coord] = self.margin + (1 - self.margin*2) * visual_pos[:, coord]
+
         color = np.zeros(self.n_target, dtype=object)
         color[:] = self.color_still
-        color[selected] = self.color_selected
+        color[self.selected] = self.color_selected
 
         radius = self.base_radius + (self.max_radius - self.base_radius) * p_val
 
@@ -191,6 +203,9 @@ class Model:
 
     def update_deriv(self):
 
+        norm_ratio = np.sum(self.var_ratio + 0.4)
+        self.var_ratio[:] = ((self.var_ratio + 0.4) / norm_ratio) * 10e4
+
         self.hist_pos = np.roll(self.hist_pos, -1)
         self.hist_pos[:, :, -1] = self.pos
 
@@ -203,50 +218,50 @@ class Model:
                 new_val = (1 - self.lag_tau) * hm + self.lag_tau * pos
                 self.hist_model[i, coord, -1] = new_val
 
-        # c = self.pos + self.control
+        var = np.zeros(2)
+        for frame in range(1, self.n_frame_var):
+            for coord in range(2):
+                c_now = self.hist_control[coord, frame]
+                c_prev_frame = self.hist_control[coord, frame - 1]
+                diff = c_now - c_prev_frame
+                var[coord] += diff ** 2
+        cvar = distance(np.zeros(2), var)
+        if cvar > 2000:
+            cvar = 2000
+        cvar /= 1000
 
         e_var = np.zeros(self.n_target)
         for i in range(self.n_target):
-            h = self.hist_pos[i]
-            m = np.mean(h, axis=-1)
             var = np.zeros(2)
             for coord in range(2):
-                diff = h[coord] - m[coord]
-                var[coord] = np.sum(diff**2, axis=-1)
+                h = self.hist_pos[i, coord, :]
+                diff = h - h.mean()
+                var[coord] = (diff**2).sum()
             e_var[i] = distance(np.zeros(2), var)
 
         a_var = np.zeros(self.n_target)
         for i in range(self.n_target):
-            hc, hm = self.hist_control[i], self.hist_model[i]
-            add = hc + hm
-            var = np.sum(add**2)
+            var = np.zeros(2)
+            for coord in range(2):
+                hc = self.hist_control[coord, :]
+                hm = self.hist_model[i, coord, :]
+                add = hc + hm
+                var[coord] = (add**2).sum()
             a_var[i] = distance(np.zeros(2), var)
 
+        var_rat = 1.0 - np.sqrt((e_var+22000) / a_var+1e-5)
+        var_rat += cvar
+
         for i in range(self.n_target):
-            var = 0
-            for j in range(1, self.n_frame_var):
-                c_now = self.hist_control[i, j]
-                c_prev_frame = self.hist_control[i, j - 1]
-                diff = c_now - c_prev_frame
-                var += diff**2
 
-            cvar = distance(np.zeros(2), var)
-
-            var_rat = 1.0 - np.sqrt((e_var[i]+22000) / a_var[i]+1e-5)
-
-            if cvar > 2000:
-                cvar = 2000
-
-            var_rat += cvar/1000
-
-            if var_rat < self.add_threshold:
-                self.var_ratio[i] += var_rat * self.add_coeff
-            elif var_rat > self.decay_threshold:
+            if var_rat[i] < self.add_threshold:
+                self.var_ratio[i] += var_rat[i] * self.add_coeff
+            elif var_rat[i] > self.decay_threshold:
                 self.var_ratio[i] /= self.decay_coeff
 
-        # np.clip(self.var_ratio, self.var_ratio_min, self.var_ratio_max)
-
     def update_wave(self):
+
+        self.disturbance_phase += self.phase_inc
 
         disturbance = \
             (np.sin(self.disturbance_phase * self.freq
@@ -261,34 +276,6 @@ class Model:
             self.pos[i] += total_d
 
         self.pos = np.clip(self.pos, -100, 100)
-
-    def normalize(self):
-
-        norm_ratio = np.sum(self.var_ratio + 0.4)
-        self.var_ratio[:] = ((self.var_ratio + 0.4) / norm_ratio) * 10e4
-
-    # def update_graphics(self, pos, delta_mouse, delta_noise):
-    #
-    #     for i in range(self.n_target):
-    #
-    #         Circle(window=self.window,
-    #                position=position, color=self.color[i],
-    #                radius=self.radius[i]).draw()
-            # Circle(window=self.window, position=pos[i], color=self.color[i],
-            #        radius=self.max_radius,
-            #        width=self.width_circle_line).draw()
-
-            # Line(window=self.window,
-            #      color="black",
-            #      start_position=pos[i],
-            #      stop_position=pos[i] + delta_mouse * self.line_scale).draw()
-            #
-            # Line(window=self.window,
-            #      color="red",
-            #      start_position=pos[i],
-            #      stop_position=pos[i] + delta_noise * self.line_scale).draw()
-
-        # self.window.update()
 
 
 def main():
