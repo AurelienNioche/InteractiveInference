ec52cc8c28427ca96440654dbc7198eed32e88d8
Sebastian Stein <sebastian.stein@glasgow.ac.uk>
2023-03-31 17:38:25 +0100

    Implement fully observed active inference by gradient descent.

--- src/hello_world/minimal_agent_jax.py
+++ src/hello_world/minimal_agent_jax.py
@@ -4,15 +4,12 @@ import numpy as np
 import jax
 import jax.numpy as jnp
 
-jax_key = jax.random.PRNGKey(42)
 print(f"JAX devices: {jax.devices()}")
 print(f"JAX device type: {jax.devices()[0].device_kind}")
 
 # check GPU status
 def get_gpu_memory():
     print(sp.check_output("nvidia-smi").decode('ascii'))
-    
-get_gpu_memory()
 
 def jax_safelog(x):
   return jnp.log( jnp.maximum(x, 1e-16 ))
@@ -111,10 +108,8 @@ class MinimalAgentJax:
         
     def reset(self):
         # initialize state preference
-        q_star = np.eye(self.env.s_N)[self.target_state] \
-                 + 1/(self.env.s_N*5) * np.ones(self.env.s_N)
-        self.q_star = q_star / q_star.sum()
-        self.log_p_c = jnp.log( self.q_star )
+        q_star = np.eye(self.env.s_N)[self.target_state] * 10
+        self.log_p_c = q_star
         # initialize state prior as uniform
         self.q = jnp.asarray(np.ones(self.env.s_N) / self.env.s_N )
     
@@ -182,11 +177,8 @@ class FullyObservedAgentJax:
         
     def reset(self):
         # initialize state preference
-        q_star = np.eye(self.env.s_N)[self.target_state] \
-                 + 1/(self.env.s_N*5) * np.ones(self.env.s_N)
-        q_star = q_star / q_star.sum()
-        self.log_p_c = jnp.log( q_star )
-        # initialize state prior as uniform
+        q_star = np.eye(self.env.s_N)[self.target_state] * 10
+        self.log_p_c = q_star
     
     def step(self, o):
         params = {
@@ -196,3 +188,67 @@ class FullyObservedAgentJax:
             'πs': self.πs, 
         }
         return jit_fully_observed_batched_fun(**params)
+    
+    
+def jax_fully_observed_nefe(π_logits, q, p_t, log_p_c):
+    """ Deterministic policy selection with pragmatic value in fully-observed environment.
+    Args:
+        num_actions (int): number of unique actions. Jit assumes this is static across calls.
+        q (jnp.ndarray): one-hot encoding of the observed state
+        πs (jnp.ndarray): 4D tensor of one-hot actions [batch, policy, timestep, one_hot_action].
+        p_t (jnp.ndarray): 3D tensor of transition dynamics [a, s0, s1].
+        log_p_c (jnp.ndarray): 1D tensor of normalized state preferences. One element per environment state.
+    """
+
+    def step(q, a):
+        q = a @ (q @ p_t)
+        return q, q
+    
+    # calculate nefe
+    π = jax.nn.softmax(π_logits)
+    qs_π = jax.lax.scan(step, init=q, xs=π)[1] # carry, output
+    pragmatic = (qs_π @ log_p_c).sum(axis=-1)
+    nefe = pragmatic
+    return nefe
+
+def jax_update(π_logits, q, p_t, log_p_c, lr):
+    eval_fun = jax.value_and_grad(jax_fully_observed_nefe)
+    v, g = eval_fun(π_logits, q, p_t, log_p_c)
+    π_logits = π_logits + lr * g
+    return v, π_logits
+
+class FullyObservedGradAgentJax:
+    """ Minimal agent performing exact inference in fully discrete POMDPs"""
+    def __init__(self, 
+                 env,
+                 target_state, 
+                 k=2, 
+                 num_grad_steps=100, 
+                 learning_rate=2.):
+        self.env = env
+        self.target_state = target_state
+        self.k = k
+        self.p_t = jnp.asarray(env.p_s1_given_s_a.swapaxes(0,1))
+        self.num_gradient_steps = num_grad_steps
+        self.learning_rate = learning_rate
+        
+    def reset(self):
+        # initialize state preference
+        q_star = np.eye(self.env.s_N)[self.target_state] * 10
+        self.log_p_c = jnp.log( jnp.exp( q_star ))
+    
+    def step(self, o, debug=True):
+        π_logits = jnp.zeros( (self.k, self.env.a_N) ) # initialize uniform
+        params = {
+            'q': jax.nn.one_hot(o, self.env.s_N),
+            'p_t': self.p_t,
+            'log_p_c': self.log_p_c,
+            'lr': self.learning_rate
+        }
+        vv = []
+        for _ in range(self.num_gradient_steps):
+            v, π_logits = jax.jit(jax_update)(π_logits, **params)
+            vv.append(v)
+        
+        π = π_logits.argmax(axis=-1)
+        return π, vv, π_logits if debug else π
